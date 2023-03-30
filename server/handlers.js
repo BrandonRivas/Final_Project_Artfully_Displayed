c4b36c2c13bccd61088aee69770c440209ed5ee3
“Brandon <brandon.a.ri@hotmail.com>
2023-03-29 23:07:47 -0400

    A basic Fetch

--- server/handlers.js
+++ server/handlers.js
@@ -1,12 +1,23 @@
 "use strict";
 const { MongoClient } = require("mongodb");
+const express = require("express");
 
 require("dotenv").config({ path: "../.env" });
 
 const MONGO_URI = process.env.MONGO_URI;
+const MUSEUM_KEY = process.env.MUSEUM_API_KEY;
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-
-module.exports = {};
+express();
+const getCollection = (request, response, next) => {
+  fetch(`https://www.rijksmuseum.nl/api/en/collection?key=${MUSEUM_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+      response.json(data);
+    })
+    .catch(next);
+};
+module.exports = { getCollection };
